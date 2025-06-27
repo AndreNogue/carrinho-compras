f0a654f7c0fb9b81bddce3a85c7ecf29d8f696a8
DreNogue <andrenogueira619@gmail.com>
2025-06-27 16:23:06 -0300

    Refatoração do código para melhorar a legibilidade e a consistência

--- js/app.js
+++ js/app.js
@@ -1,36 +1,34 @@
-
-let totalGeral = 0
+let totalGeral = 0;
 limpar();
 
-
 function adicionar() {
-    // Recuperar valores: nome do produto , quantridade e valor
-    let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
-    // Calcular o preço, o subtotal
-    let preco = quantidade * valorUnitario;
-    // Adicionar o prod no carrinho 
-    let listaCarrinho = document.getElementById('lista-produtos');
-    listaCarrinho.innerHTML = listaCarrinho.innerHTML +
-        `<section class="carrinho__produtos__produto">
+  // Recuperar valores: nome do produto , quantridade e valor
+  let produto = document.getElementById("produto").value;
+  let nomeProduto = produto.split("-")[0];
+  let valorUnitario = produto.split("R$")[1];
+  let quantidade = document.getElementById("quantidade").value;
+  
+  // Calcular o preço, o subtotal
+  let preco = quantidade * valorUnitario;
+
+  // Adicionar o prod no carrinho
+  let listaCarrinho = document.getElementById("lista-produtos");
+  listaCarrinho.innerHTML =
+    listaCarrinho.innerHTML +
+    `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-    </section>`
+    </section>`;
 
-    // Atualizar o total da compra
-    totalGeral = totalGeral + preco;
-    let campoTotal = document.getElementById('valor-total');
-    campoTotal.textContent = `R$${totalGeral}`;
-    document.getElementById('quantidade').value = 0;
-
-
+  // Atualizar o total da compra
+  totalGeral = totalGeral + preco;
+  let campoTotal = document.getElementById("valor-total");
+  campoTotal.textContent = `R$${totalGeral}`;
+  document.getElementById("quantidade").value = 0;
 }
 
-
 function limpar() {
-    // Limpar o carrinho
-    totalGeral = 0
-    document.getElementById('lista-produtos').innerHTML = 0;
-    document.getElementById('valor-total').textContent = 'R$0';
+  // Limpar o carrinho
+  totalGeral = 0;
+  document.getElementById("lista-produtos").innerHTML = 0;
+  document.getElementById("valor-total").textContent = "R$0";
 }
