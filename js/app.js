5036a8fc64db092e892e408bfacec98092290789
DreNogue <andrenogueira619@gmail.com>
2025-06-27 16:19:33 -0300

    Melhorando as funcionalidades do app

--- js/app.js
+++ js/app.js
@@ -1,27 +1,37 @@
+
+let totalGeral = 0
+limpar();
+
+
 function adicionar(){
     // Recuperar valores: nome do produto , quantridade e valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
     // Calcular o preço, o subtotal
-    let preco = quantidade.value * valorUnitario;
+    let preco = quantidade * valorUnitario;
     // Adicionar o prod no carrinho 
     let listaCarrinho = document.getElementById('lista-produtos');
-    listaCarrinho.innerHTML = 
+    listaCarrinho.innerHTML = listaCarrinho.innerHTML +
     `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade.value}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
     </section>`
+
     // Atualizar o total da compra
-    let total = document.getElementsByClassName('carrinho__total');
-    total[0].innerHTML = `Total: <span class="texto-azul" id="valor-total">R$${preco}</span>`
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+
+
 }
 
 
 function limpar(){
     // Limpar o carrinho
-    let listaCarrinho = document.getElementById('lista-produtos');
-    listaCarrinho.innerHTML = '';
-    let total = document.getElementsByClassName('carrinho__total');
-    total[0].innerHTML = '';
+   totalGeral = 0
+   document.getElementById('lista-produtos').innerHTML = 0;
+   document.getElementById('valor-total').textContent = 'R$0';
+
 }
